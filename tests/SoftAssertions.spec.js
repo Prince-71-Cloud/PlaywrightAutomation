2bfd9d9493ebc71468cdc57b0614d7eae548b9ab
Aman Bhuiyan <bhuiyanaman71@email.com>
2025-08-13 16:51:56 +0600

    soft assertion file modify

--- tests/SoftAssertions.spec.js
+++ tests/SoftAssertions.spec.js
@@ -1,9 +1,10 @@
 import {test, expect} from '@playwright/test'
 // @ts-check
 test('Soft asseritons practice', async ({page})=>{
-    await page.goto('https://demo.nopcommerce.com/');
+    await page.goto('https://www.daraz.pk/#?');
+
     // soft assertion (.soft)
-    await expect.soft(page).toHaveTitle('nopCommerce demo store. Home page title123');//use soft assertion for ignoring failed test case
-    await expect(page).toHaveURL('https://demo.nopcommerce.com/');
-    await expect(page.locator('.header-logo')).toBeVisible();
+    await expect.soft(page).toHaveTitle('Online Shopping in Pakistan: Fashion, Electronics & Groceries - Daraz.pk');//use soft assertion for ignoring failed test case
+    await expect(page).toHaveURL('https://www.daraz.pk/#?');
+    await expect(page.locator('.lzd-logo-content')).toBeVisible();
 })
